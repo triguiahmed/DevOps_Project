ef2996bb702f26c1f38310c7303665251cf9e5ec
mohamed hadiji <mohamed.hadiji@esprit.tn>
2024-04-03 22:22:10 +0100

    first ut

---
+++ DevOps_Project/src/test/java/tn/esprit/devops_project/DevOps_ProjectSpringBootApplicationTest.java
@@ -0,0 +1,14 @@
+package tn.esprit.devops_project;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class DevOps_ProjectSpringBootApplicationTest {
+        @Test
+        void contextLoads() {
+        }
+    }
+
