bfb97972fdc644e69451f63e98eca8a4c7ebce0c
MedAziz11 <58366672+MedAziz11@users.noreply.github.com>
2024-04-04 02:08:04 +0100

    Update product.service.ts

--- DevOps_Project_Front/src/app/services/product.service.ts
+++ DevOps_Project_Front/src/app/services/product.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ProductService {
-  private apiUrl = 'http://backend:8082'; 
+  private apiUrl = 'http://localhost:8083'; 
 
   constructor(private http: HttpClient) { }
 
